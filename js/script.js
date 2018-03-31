d15362337ec1244f7fcc4868ce39ee45015d2060
Alex <36758083+Destors@users.noreply.github.com>
2018-03-31 23:39:45 +0300

    fix_4.7

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
   var link = document.querySelector(".contacts-btn");
 
   var popup = document.querySelector(".modal-window");
+
   var close = popup.querySelector(".modal-close-btn");
+
+  var login = popup.querySelector("[name=username]");
 
   link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("modal-show");
+    username.focus();
   });
 
   close.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("modal-show");
+  });
+
+ window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+      }
+    }
   });
